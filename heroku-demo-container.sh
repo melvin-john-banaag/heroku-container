3ba4704507f10756152b03a8ee10c155bd174a6d
melvin-john-banaag <melvin.john.banaag@gmail.com>
2020-06-26 13:08:52 +0800

    Update heroku-demo-container.sh

--- heroku-demo-container.sh
+++ heroku-demo-container.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo 'Removing existing Heroku-App Container'
-docker container rm $(docker container ls -aqf name=heroku-demo-container)
-
 echo 'Building Dockerfile'
 docker build -t melbsmelbs/heroku-container:latest .
 
